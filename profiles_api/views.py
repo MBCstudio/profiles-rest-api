e41128d0285c60c24e913ca06ef59ed4babe1f51
Marcin Borkowski <cinek20032@wp.pl>
2022-11-02 00:05:41 +0100

    Dodanie metod POST, PUT, PATH i DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HelloAPI(APIView):
     """przykład działanie klasy APIView"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request,format=None):
         """Zwraca przykładową liste API"""
@@ -13,3 +17,26 @@ class HelloAPI(APIView):
         ]
 
         return Response({'message':"Hello!", 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """tworzenie wiadomosci pocztkowej z imieniem"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('imie')
+            massage = f"Hello {name}"
+            return Response({'massage': massage})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """do updatów objektow"""
+        return Response({'method':'PUT'})
+
+    def path(self, request, pk=None):
+        """do robienia updatów objektow ale czesciowych"""
+        return Response({'method':'PATH'})
+
+    def delete(self, request, pk=None):
+        """do usuwania odjektow"""
+        return Response({'method': 'DELETE'})
