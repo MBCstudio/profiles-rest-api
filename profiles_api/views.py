7ef4fa7fbff0ddea243ad70d5ea5a3771c48b57d
Marcin Borkowski <cinek20032@wp.pl>
2022-11-01 20:49:24 +0100

    First API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPI(APIView):
+    """przykład działanie klasy APIView"""
+
+    def get(self, request,format=None):
+        """Zwraca przykładową liste API"""
+        an_apiview = [
+            "Coś tam wpisałem",
+            "Napis nr.2",
+            "Próba nr.3",
+        ]
+
+        return Response({'message':"Hello!", 'an_apiview':an_apiview})
